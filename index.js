c0d4a05fdf2586c063c2bab66ca4cacf37b3c25d
Chris Oyler <christopher.oyler@gmail.com>
2015-05-25 00:22:48 -0700

    removed some aphrodite restrictions

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 var Hoek = require('hoek');
 var Server = require('./lib/server');
 
-Server.init(function(err, server){
-  Hoek.assert(!err, err);
-  console.log('--------------------------------------------------------------------------------');
-  console.log('Hapi:\n', server.info.uri);
-  console.log('Environment:\n', server.app.environment);
-  console.log('--------------------------------------------------------------------------------');
-});
+// Server.init(function(err, server){
+//   Hoek.assert(!err, err);
+//   console.log('--------------------------------------------------------------------------------');
+//   console.log('Hapi:\n', server.info.uri);
+//   console.log('Environment:\n', server.app.environment);
+//   console.log('--------------------------------------------------------------------------------');
+// });
